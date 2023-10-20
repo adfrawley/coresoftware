118e0577832c0dff183feb5df366467dcd736d9a
Anthony Denis Frawley <afrawley@fsu.edu>
2023-10-19 23:26:44 -0400

    Convert TNtuple to TTree so hitsetkey can be added. Add creation of parameter histograms vs layer and sensor, suitable for plotting by detailed_alignment_parameter_plots.C.

--- offline/packages/TrackingDiagnostics/helixResiduals.cc
+++ offline/packages/TrackingDiagnostics/helixResiduals.cc
@@ -4,10 +4,15 @@
 #include <phool/PHCompositeNode.h>
 #include <phool/getClass.h>
 #include <trackbase/TrkrDefs.h>
+#include <trackbase/MvtxDefs.h>
+#include <trackbase/InttDefs.h>
+#include <trackbase/TpcDefs.h>
+#include <micromegas/MicromegasDefs.h>
 #include <trackbase/TrackFitUtils.h>
 
 #include <TFile.h>
-#include <TNtuple.h>
+#include <TTree.h>
+#include <TH2D.h>
 
 helixResiduals::helixResiduals(const std::string &name)
   : SubsysReco(name)
@@ -18,8 +23,60 @@ int helixResiduals::InitRun(PHCompositeNode *topNode)
 {
   const char *cfilepath = filepath.c_str();
   fout = new TFile(cfilepath, "recreate");
-  ntp_residuals = new TNtuple("ntp_residuals", "Seed Residuals", "seed_id:layer:nclus:dphi:dx:dy:dz:pcax:pcay:pcaz:x:y:z:pt:px:py:pz:crossing:isSilicon:isTpc");
+  tree_residuals = new TTree("tree_residuals", "Seed Residuals");
+  tree_residuals->Branch("cluskey",&cluskey,"cluskey/l");
+   tree_residuals->Branch("hitsetkey",&hitsetkey,"hitsetkey/i");
+  tree_residuals->Branch("seed_id",&seed_id,"seed_id/I");
+  tree_residuals->Branch("crossing",&crossing,"crossing/I");
+  tree_residuals->Branch("isTpc",&isTpc,"isTpc/I");
+  tree_residuals->Branch("layer",&layer,"layer/i");
+  tree_residuals->Branch("nclus",&nclus,"nclus/I");
+  tree_residuals->Branch("clusx",&clusx,"clusx/F");
+  tree_residuals->Branch("clusy",&clusy,"clusy/F");
+  tree_residuals->Branch("clusz",&clusz,"clusz/F");
+  tree_residuals->Branch("pcax",&pcax,"pcax/F");
+  tree_residuals->Branch("pcay",&pcay,"pcay/F");
+  tree_residuals->Branch("pcaz",&pcaz,"pcaz/F");
+  tree_residuals->Branch("pt",&pt,"pt/F");
+  tree_residuals->Branch("px",&px,"px/F");
+  tree_residuals->Branch("py",&py,"py/F");
+  tree_residuals->Branch("pz",&pz,"pz/F");
 
+  for(int ilayer=0;ilayer<57;++ilayer)
+    {
+      for(int ipar = 0; ipar < 6; ++ipar)
+	{
+	  double range = 1.0;  // mm
+	  double range_angles = 0.03;  // rad
+	  if( ilayer > 6 && ilayer < 56 )
+	    {
+	      if(ipar == 5)
+		range = 3.0;
+	      else 
+		range = 2.0;
+	    }
+	  if(ipar < 3) range = range_angles;
+	  
+	  char name[500];
+	  char title[500];
+	  sprintf(name,"hpar_%i_%i", ilayer, ipar);
+	  if(ilayer < 3)  sprintf(title,"MVTX parameter %i", ipar);
+	  else if (ilayer > 2 && ilayer < 7) sprintf(title,"INTT parameter %i", ipar);
+	  else if (ilayer > 6 && ilayer < 55) sprintf(title,"TPC parameter %i", ipar);
+	  else  sprintf(title,"MMS parameters %i", ipar);
+	  
+	  hpar[ilayer][ipar] = new TH2D(name, title, 600, 0, 200, 2000, -range, +range);  // sensor number, parameter range
+	  
+	  hpar[ilayer][ipar]->GetXaxis()->SetNdivisions(504);
+	  hpar[ilayer][ipar]->GetXaxis()->SetLabelSize(0.05);
+	  hpar[ilayer][ipar]->GetXaxis()->SetTitleSize(0.05);
+	  if(ipar < 3)
+	    hpar[ilayer][ipar]->GetXaxis()->SetTitle("sensor");
+	  else
+	    hpar[ilayer][ipar]->GetXaxis()->SetTitle("sensor");	    	    
+	}
+    }
+ 
   getNodes(topNode);
 
   return 0;
@@ -28,7 +85,13 @@ int helixResiduals::InitRun(PHCompositeNode *topNode)
 int helixResiduals::End(PHCompositeNode * /**topNode*/)
 {
   fout->cd();
-  ntp_residuals->Write();
+  tree_residuals->Write();
+  for(int lyr = 0; lyr < 57; ++lyr)
+    for(int ipar=0; ipar<6; ++ipar)
+      {
+	hpar[lyr][ipar]->Write();
+      }
+
   fout->Close();
 
   return 0;
@@ -95,7 +158,7 @@ int helixResiduals::process_event(PHCompositeNode * /*topNode*/)
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
-void helixResiduals::fill_residuals(TrackSeed *seed, int seed_id, bool isTpc)
+void helixResiduals::fill_residuals(TrackSeed *seed, int id, bool tpc)
 {
   if (seed->size_cluster_keys() == 0)
   {
@@ -105,10 +168,10 @@ void helixResiduals::fill_residuals(TrackSeed *seed, int seed_id, bool isTpc)
   std::vector<Acts::Vector3> clusterPositions;
   std::vector<TrkrDefs::cluskey> clusterKeys;
   getTrackletClusterList(seed, clusterKeys); 
-  auto nclus = clusterKeys.size();
-  // A silicon seed with 3 clusters is not useful
-  // the helical fit will be close to perfect if there are only 3 clusters
-  if(nclus < 5) return;
+  nclus = clusterKeys.size();
+  // A silicon seed with 3 or 4 clusters is not useful, the helical fit will be close to perfect if there are only 3 clusters
+  if(!tpc && nclus < 5) return;
+  if(tpc && nclus < 20) return;
 
  TrackFitUtils::getTrackletClusters(tGeometry, _clusters, clusterPositions, clusterKeys);   
   std::vector<float> fitparams = TrackFitUtils::fitClusters(clusterPositions, clusterKeys);
@@ -118,20 +181,23 @@ void helixResiduals::fill_residuals(TrackSeed *seed, int seed_id, bool isTpc)
       return;
     }
 
-  float pt = seed->get_pt();
-  float px = seed->get_px(_clusters, tGeometry);
-  float py = seed->get_py(_clusters, tGeometry);
-  float pz = seed->get_pz();
-  unsigned int crossing = seed->get_crossing();
+  pt = seed->get_pt();
+  px = seed->get_px(_clusters, tGeometry);
+  py = seed->get_py(_clusters, tGeometry);
+  pz = seed->get_pz();
+  crossing = seed->get_crossing();
+  seed_id = id;
 
   for (size_t i = 0; i < clusterPositions.size(); i++)
   {
-    unsigned int layer = TrkrDefs::getLayer(clusterKeys[i]);
+    layer = TrkrDefs::getLayer(clusterKeys[i]);
+    cluskey = clusterKeys[i];
+    hitsetkey = TrkrDefs::getHitSetKeyFromClusKey(clusterKeys[i]);
     Acts::Vector3 position = clusterPositions[i];
    Acts::Vector3 pca = TrackFitUtils::get_helix_pca(fitparams, position);
-    float cluster_phi = atan2(position(1), position(0));
-    float pca_phi = atan2(pca(1), pca(0));
-    float dphi = cluster_phi - pca_phi;
+   float cluster_phi = atan2(position(1), position(0));
+   float pca_phi = atan2(pca(1), pca(0));
+   float dphi = cluster_phi - pca_phi;
     if (dphi > M_PI)
     {
       dphi = 2 * M_PI - dphi;
@@ -140,24 +206,41 @@ void helixResiduals::fill_residuals(TrackSeed *seed, int seed_id, bool isTpc)
     {
       dphi = 2 * M_PI + dphi;
     }
-    float dx = position(0) - pca(0);
-    float dy = position(1) - pca(1);
-    float dz = position(2) - pca(2);
-    float pcax = pca(0);
-    float pcay = pca(1);
-    float pcaz = pca(2);
-    float x = position(0);
-    float y = position(1);
-    float z = position(2);
+
+    pcax = pca(0);
+    pcay = pca(1);
+    pcaz = pca(2);
+    clusx = position(0);
+    clusy = position(1);
+    clusz = position(2);
+    isTpc = tpc;
 
     if(Verbosity() > 0)
       {
+	float dx = position(0) - pca(0);
+	float dy = position(1) - pca(1);
+	float dz = position(2) - pca(2);
 	std::cout << "   layer " << layer << " nclus " << nclus << " position(microns) " << position(0)*10000 << "  " << position(1)*10000 << "  " << position(2)*10000 << " pca " << pca(0)*10000 << "  " << pca(1)*10000 << "  " << pca(2)*10000 << " dx " << dx*10000 << "  dy " << dy*1000 << " dz " << dz*10000 << std::endl;
       }
 
-    float data[20] = {(float)seed_id, (float)layer, (float)nclus, dphi, dx, dy, dz, pcax, pcay, pcaz, x, y, z, pt, px, py, pz, (float)crossing, (float)!isTpc, (float)isTpc};
+    tree_residuals->Fill();
 
-    ntp_residuals->Fill(data);
+    // make a 2D histogram suitable for plotting by "detailed_alignment_parameter_plots.C" 
+
+    // convert to mm for plotting
+    clusx *= 10; clusy *= 10; clusz *= 10;
+    pcax *= 10; pcay *= 10; pcaz *= 10;
+
+    unsigned int sensor = getSensor(hitsetkey);
+    float pars[6] = {0,0,0,0,0,0};
+    pars[3] = clusx-pcax;
+    pars[4] = clusy-pcay;
+    pars[5] = clusz-pcaz;
+    
+    for(int ipar=0;ipar<6;++ipar)
+      {
+	hpar[layer][ipar]->Fill(sensor, pars[ipar]);
+      }
   }
 }
 
@@ -184,10 +267,45 @@ void helixResiduals::getTrackletClusterList(TrackSeed *tracklet, std::vector<Trk
 	}
 
       // drop some bad layers in the TPC completely
-      unsigned int layer = TrkrDefs::getLayer(key);
-      if(layer == 7 || layer == 22 || layer == 23 || layer == 38 || layer == 39) {continue;}
+      unsigned int lyr = TrkrDefs::getLayer(key);
+      if(lyr == 7 || lyr == 22 || lyr == 23 || lyr == 38 || lyr == 39) {continue;}
 
       cluskey_vec.push_back(key);
       
     } // end loop over clusters for this track 
+}
+
+
+unsigned int helixResiduals::getSensor(TrkrDefs::hitsetkey hskey)
+{
+  unsigned int sensor = 0;
+  unsigned int trkrid = TrkrDefs::getTrkrId(hskey);
+
+  if(trkrid == TrkrDefs::mvtxId)
+    {
+      unsigned int staveid = MvtxDefs::getStaveId(hskey);
+      unsigned int chipid = MvtxDefs::getChipId(hskey);
+      sensor = staveid*9 + chipid;
+    }
+  else if(trkrid == TrkrDefs::inttId)
+    {
+      unsigned int ladderzid = InttDefs::getLadderZId(hskey);
+      unsigned int ladderphiid = InttDefs::getLadderPhiId(hskey);
+      sensor = ladderphiid*4 + ladderzid;      
+    }
+  else if(trkrid == TrkrDefs::tpcId)
+    {
+      unsigned int sector = TpcDefs::getSectorId(hskey);
+      unsigned int side = TpcDefs::getSide(hskey);
+      sensor = side * 12 + sector; 
+    }
+  else
+    {
+      // trkrid == TrkrDefs::micromegasId
+      unsigned int tile = MicromegasDefs::getTileId(hskey);
+      sensor = tile;
+    }
+
+  return sensor;
+
 }
--- offline/packages/TrackingDiagnostics/helixResiduals.h
+++ offline/packages/TrackingDiagnostics/helixResiduals.h
@@ -3,6 +3,7 @@
 
 #include <trackbase/ActsTrackingGeometry.h>
 #include <trackbase/TrkrClusterContainerv4.h>
+#include <trackbase/TrkrDefs.h>
 #include <trackbase_historic/SvtxTrackMap.h>
 #include <trackbase_historic/TrackSeedContainer_v1.h>
 #include <trackbase_historic/TrackSeed_v1.h>
@@ -10,7 +11,8 @@
 #include <fun4all/SubsysReco.h>
 
 class TFile;
-class TNtuple;
+class TTree;
+class TH2D;
 
 class PHCompositeNode;
 class SvtxTrack;
@@ -32,10 +34,11 @@ class helixResiduals : public SubsysReco
 
   void fill_residuals(TrackSeed* seed, int seed_id, bool isTpc);
   void getTrackletClusterList(TrackSeed *tracklet, std::vector<TrkrDefs::cluskey>& cluskey_vec);
+  unsigned int getSensor(TrkrDefs::hitsetkey hitsetkey);
 
   ActsGeometry* tGeometry = nullptr;
 
-  TNtuple* ntp_residuals = nullptr;
+  //  TNtuple* ntp_residuals = nullptr;
 
   SvtxTrackMap* _tracks = nullptr;
   TrkrClusterContainer* _clusters = nullptr;
@@ -45,6 +48,17 @@ class helixResiduals : public SubsysReco
   TFile* fout = nullptr;
 
   std::string filepath = "";
+
+  float clusx,clusy,clusz,pcax,pcay,pcaz,pt,px,py,pz;
+  int seed_id, isTpc,nclus;
+  int crossing;
+  unsigned int layer;
+  TrkrDefs::cluskey cluskey;
+  TrkrDefs::hitsetkey hitsetkey;
+
+  TTree *tree_residuals;
+  TH2D *hpar[57][6];
+
 };
 
 #endif  // HELIXRESIDUALS_H
